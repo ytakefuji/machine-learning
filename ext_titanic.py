f5eb6861cb4ea7576c10298d5acb1a544aa3aaf8
ytakefuji <ytakefuji@gmail.com>
2019-01-07 15:12:33 +0900

    Update ext_titanic.py

--- ext_titanic.py
+++ ext_titanic.py
@@ -1,25 +1,21 @@
 import pandas as pd
 titanic=pd.read_csv('titanic.csv',encoding="shift-jis")
-
-from sklearn.feature_extraction import DictVectorizer
-def one_hot_dataframe(data, cols, replace=False):
-    vec = DictVectorizer(sparse=False)
-    vecData = pd.DataFrame(vec.fit_transform(data[cols].T.to_dict().values()))
-    vecData.columns = vec.get_feature_names()
-    vecData.index = data.index
-    if replace is True:
-        data = data.drop(cols, axis=1)
-        data = data.join(vecData)
-    return (data)
-
-titanic=one_hot_dataframe(titanic,['pclass','embarked','sex','home.dest','room','ticket','boat'],replace=True)
+titanic=titanic.drop(['name','row.names'],axis=1)
 mean=titanic['age'].mean()
-titanic['age'].fillna(mean,inplace=True)
 titanic.fillna(0,inplace=True)
+
+from sklearn import preprocessing
+le = preprocessing.LabelEncoder()
+titanic=titanic.apply(le.fit_transform)
+titanic['age'].fillna(mean,inplace=True)
 from sklearn.model_selection import train_test_split
 titanic_target = titanic['survived']
-titanic_data=titanic.drop(['name','row.names','survived'],axis=1)
-X_train,X_test,y_train,y_test=train_test_split(titanic_data,titanic_target,test_size=0.3,random_state=54,shuffle=True)
+titanic_data=titanic.drop(['survived'],axis=1)
+yX=titanic_target
+yX=pd.concat([yX,titanic_data],axis=1)
+yX.to_csv('temp.csv',encoding='utf-8')
+
+X_train,X_test,y_train,y_test=train_test_split(titanic_data,titanic_target,test_size=0.2,random_state=54,shuffle=True)
 from sklearn.ensemble import ExtraTreesClassifier
 clf=ExtraTreesClassifier(n_estimators=382, max_depth=None,min_samples_split=2,random_state=0)
 clf.fit(X_train,y_train)
@@ -27,3 +23,6 @@ y_pred=clf.predict(X_test)
 from sklearn import metrics
 print (metrics.accuracy_score(y_test,y_pred))
 print(clf.score(X_test,y_test))
+dic=dict(zip(titanic_data.columns,clf.feature_importances_))
+for item in sorted(dic.items(), key=lambda x: x[1], reverse=True):
+    print(item[0],round(item[1],4))
