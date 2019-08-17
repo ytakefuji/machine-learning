091190464e3f72cc43a5c141f81fee4186e1ed09
Yoshiyasu Takefuji <ytakefuji@gmail.com>
2019-08-17 18:52:34 +0900

    Add files via upload

--- randfS_pima.py
+++ randfS_pima.py
@@ -6,19 +6,18 @@ y = pima['diabetes']
 X=pima.drop(['diabetes'],axis=1)
 
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=54,shuffle=True)
+
+from imblearn.over_sampling import SMOTE
+smt = SMOTE(random_state=90)
+X_train,y_train= smt.fit_resample(X_train,y_train)
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import *
 clf=RandomForestClassifier(n_estimators=382, max_depth=None,min_samples_split=2,random_state=8)
 clf.fit(X_train,y_train)
-print(clf.score(X_test,y_test))
-dic=dict(zip(X.columns,clf.feature_importances_))
-for item in sorted(dic.items(), key=lambda x: x[1], reverse=True):
-    print(item[0],round(item[1],4))
-'''
 y_pred=clf.predict(X_test)
-from sklearn.metrics import *
-print("acc:",'%.4f'%accuracy_score(y_test,y_pred))
+print("acc:",'%.4f'%clf.score(X_test,y_test))
 print(confusion_matrix(y_test, y_pred))
 print('f1:','%.4f'%f1_score(y_test, y_pred))
 print('recall:','%.4f'%recall_score(y_test, y_pred))
 print('precision:','%.4f'%precision_score(y_test, y_pred))
-'''
